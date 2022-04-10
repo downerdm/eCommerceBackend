e50788db03386fa391545ba33b6745b1c0e6c408
Dennis Downer <downerdm@gmail.com>
2022-04-10 15:02:34 -0400

    Product model columns defined and index modified for relationships

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,8 +5,15 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'id',
+  onDelete: 'CASCADE',
+})
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'id',
+  onDelete: 'CASCADE',
+})
 
 // Products belongToMany Tags (through ProductTag)
 
